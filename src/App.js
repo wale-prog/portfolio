3a729dcc452456338696df58ff43099062d6bcb2
Olawale <olapetanwale@outlook.com>
2022-12-14 23:20:34 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
 
-      <div className='container'>
+      <div className="container">
         <Header onSetMobileMenu={mobileMenuStatus} />
         <MobileMenu menu={mobileMenu} />
       </div>
